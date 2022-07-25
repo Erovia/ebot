82fce35c8d04fac46cd98c0fa9ebbfaa19672737
Erovia <erovia@users.noreply.github.com>
2022-07-25 20:21:53 +0100

    Disable fun features

--- ebot.py
+++ ebot.py
@@ -61,7 +61,7 @@ except json.decoder.JSONDecodeError:
 
 intents = discord.Intents.default()
 intents.message_content = True
-intents.members = True
+# intents.members = True
 
 bot = commands.Bot(intents = intents, command_prefix = commands.when_mentioned)
 
@@ -222,30 +222,30 @@ class Egg(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		self.logger = logging.getLogger('EggCog')
-		self.YUGE_REGEX = re.compile(r'[h|y]u+ge', re.IGNORECASE)
-		self.MAYBE_REGEX = re.compile(r'ma+ybe', re.IGNORECASE)
+		# self.YUGE_REGEX = re.compile(r'[h|y]u+ge', re.IGNORECASE)
+		# self.MAYBE_REGEX = re.compile(r'ma+ybe', re.IGNORECASE)
 		self.COCOA_REGEX = re.compile(r'(^|\s)cocoa', re.IGNORECASE)
-		self.BAN_REGEX = re.compile(r'(^|\s)ban(\s|$)', re.IGNORECASE)
+		# self.BAN_REGEX = re.compile(r'(^|\s)ban(\s|$)', re.IGNORECASE)
 		self.SENTIENCE_REGEX = re.compile(r'[Aa]re\s+[Yy]ou\s+(.*)\?')
-		self.YUGE_ID = 'l0HefZY0mFfLS9AFa'
-		self.MAYBE_ID = 'gZGlQX3wWAV1u'
+		# self.YUGE_ID = 'l0HefZY0mFfLS9AFa'
+		# self.MAYBE_ID = 'gZGlQX3wWAV1u'
 		self.SEAGAL_IDS = ('EjFx0jioOoMZq', 'CeMJ1CHY7CJMI', '53JRR3jD18vCw',
                        'fs9BuKuLs7CQWerV1q', '3oz8xyu5a15nCQafq8', 'RxzDe6KVv9SJW',
                        '26gJzhfjTvrFu4JuE', '3o6ZsYxFRLqno054GI', 'l0HlxPSpyojsTLjqM',
                        'l0HlwcbDDfN8I9vhK', 'l0HlPhK8pemiFgF4k', '95wZBAoDFCNYQ',
                        'l0HlJXtvGLbMQYjNS', '11BkowkONO4qGc', 'wcG2ivAWvpQs0',
                        '13FOmRwAHCqLp6', '3o6Ztqb8VuN88HvI0o', 'l0HlJXtvGLbMQYjNS')
-		self.TIT_ID = 'uSGDIb6hP458c'
-		self.BOOBY_ID = 'EExgR4RJV0CM6nfeKQ'
-		self.BAN_IDS= ['CybZqG4etuZsA', '8FJCnrkqkyRzIswceT', 'HXcALJVPgaR4A',
-                   'C51woXfgJdug', 'fe4dDMD2cAU5RfEaCU']
-		self.GOODBYE_CHANNEL = os.environ.get('GOODBYE_CHANNEL', None)
-		if self.GOODBYE_CHANNEL:
-			try:
-				self.GOODBYE_CHANNEL = int(self.GOODBYE_CHANNEL)
-			except ValueError:
-				self.GOODBYE_CHANNEL = None
-				self.logger.error('The provide ID for "GOODBYE_CHANNEL" was not its numeric ID!')
+		# self.TIT_ID = 'uSGDIb6hP458c'
+		# self.BOOBY_ID = 'EExgR4RJV0CM6nfeKQ'
+		# self.BAN_IDS= ['CybZqG4etuZsA', '8FJCnrkqkyRzIswceT', 'HXcALJVPgaR4A',
+                   # 'C51woXfgJdug', 'fe4dDMD2cAU5RfEaCU']
+		# self.GOODBYE_CHANNEL = os.environ.get('GOODBYE_CHANNEL', None)
+		# if self.GOODBYE_CHANNEL:
+		# 	try:
+		# 		self.GOODBYE_CHANNEL = int(self.GOODBYE_CHANNEL)
+		# 	except ValueError:
+		# 		self.GOODBYE_CHANNEL = None
+		# 		self.logger.error('The provide ID for "GOODBYE_CHANNEL" was not its numeric ID!')
 
 
 	@commands.Cog.listener()
@@ -269,16 +269,16 @@ class Egg(commands.Cog):
 
 			if self.COCOA_REGEX.search(message.content):
 				msg = self.grab_specific_gif(random.choice(self.SEAGAL_IDS))
-			elif self.YUGE_REGEX.search(message.content):
-				msg = self.grab_specific_gif(self.YUGE_ID)
-			elif self.MAYBE_REGEX.search(message.content):
-				msg = self.grab_specific_gif(self.MAYBE_ID)
-			elif 'tits' in message.clean_content or 'titties' in message.clean_content:
-				msg = self.grab_specific_gif(self.TIT_ID)
-			elif 'boobs' in message.clean_content or 'booby' in message.clean_content or 'boobies' in message.clean_content:
-				msg = self.grab_specific_gif(self.BOOBY_ID)
-			elif self.BAN_REGEX.search(message.content):
-				msg = self.grab_specific_gif(random.choice(self.BAN_IDS))
+			# elif self.YUGE_REGEX.search(message.content):
+			# 	msg = self.grab_specific_gif(self.YUGE_ID)
+			# elif self.MAYBE_REGEX.search(message.content):
+			# 	msg = self.grab_specific_gif(self.MAYBE_ID)
+			# elif 'tits' in message.clean_content or 'titties' in message.clean_content:
+			# 	msg = self.grab_specific_gif(self.TIT_ID)
+			# elif 'boobs' in message.clean_content or 'booby' in message.clean_content or 'boobies' in message.clean_content:
+			# 	msg = self.grab_specific_gif(self.BOOBY_ID)
+			# elif self.BAN_REGEX.search(message.content):
+			# 	msg = self.grab_specific_gif(random.choice(self.BAN_IDS))
 
 		if msg:
 			await message.channel.send(msg)
@@ -297,20 +297,20 @@ class Egg(commands.Cog):
 		return url
 
 
-	@commands.Cog.listener()
-	async def on_member_remove(self, member):
-		if self.GOODBYE_CHANNEL:
-			self.logger.info(f'{member.id} left the server')
-			gif = self.grab_random_gif('sassy')
-			channel = self.bot.get_channel(self.GOODBYE_CHANNEL)
-			# breakpoint()
-			if channel:
-				await channel.send(f'Buh-bye, {member.name}!')
-				await channel.send(gif)
-			else:
-				self.logger.error(f'Channel "{self.GOODBYE_CHANNEL}" was not found!')
-		else:
-			self.logger.info('A member left, but no "GOODBYE_CHANNEL" was configured.')
+	# @commands.Cog.listener()
+	# async def on_member_remove(self, member):
+	# 	if self.GOODBYE_CHANNEL:
+	# 		self.logger.info(f'{member.id} left the server')
+	# 		gif = self.grab_random_gif('sassy')
+	# 		channel = self.bot.get_channel(self.GOODBYE_CHANNEL)
+	# 		# breakpoint()
+	# 		if channel:
+	# 			await channel.send(f'Buh-bye, {member.name}!')
+	# 			await channel.send(gif)
+	# 		else:
+	# 			self.logger.error(f'Channel "{self.GOODBYE_CHANNEL}" was not found!')
+	# 	else:
+	# 		self.logger.info('A member left, but no "GOODBYE_CHANNEL" was configured.')
 
 
 	@commands.command()
